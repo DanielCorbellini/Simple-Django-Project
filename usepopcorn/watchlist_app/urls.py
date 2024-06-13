faca2773895c0b497167cf6fea6ae09ca4b341c7
Daniel <daniel.corbellini28@gmail.com>
2024-06-12 22:11:25 -0300

    Creating JSON Response for all elements

---
+++ usepopcorn/watchlist_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+from watchlist_app.views import movie_list
+
+urlpatterns = [
+    path('list/', movie_list, name='movie-list'),
+]
--- usepopcorn/watchlist_app/views.py
+++ usepopcorn/watchlist_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from watchlist_app.models import Movie
+from django.http import JsonResponse
 
-# Create your views here.
+
+def movie_list(request):
+    movies = Movie.objects.all()
+    data = {
+        'movies': list(movies.values())
+    }
+
+    return JsonResponse(data)
