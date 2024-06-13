8481033050b96fe0ac1e153d3a9b719060e3f241
Daniel <daniel.corbellini28@gmail.com>
2024-06-12 22:27:33 -0300

    Creating JSON Response for individual elements

--- usepopcorn/watchlist_app/urls.py
+++ usepopcorn/watchlist_app/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
-from watchlist_app.views import movie_list
+from django.urls import path
+from watchlist_app.views import movie_list, movie_details
 
 urlpatterns = [
     path('list/', movie_list, name='movie-list'),
+    path('list/<int:pk>', movie_details, name='movie-detail'),
 ]
